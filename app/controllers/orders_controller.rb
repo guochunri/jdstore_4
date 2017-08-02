3b34bad5725775e34d7a938857bbcf86a080d36b
郭春日 <uochunri@gmail.com>
2017-08-02 15:58:09 +0800

    add apply_cancel function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,15 +36,22 @@ class OrdersController < ApplicationController
       @order.make_payment!
 
       redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
-    end
+  end
 
-    def pay_with_wechat
-      @order = Order.find_by_token(params[:id])
-      @order.set_payment_with!("wechat")
-      @order.make_payment!
+  def pay_with_wechat
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("wechat")
+    @order.make_payment!
 
-      redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
-    end
+    redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
+  end
+
+  def apply_to_cancel
+    @order = Order.find_by_token(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "已提交申请"
+    redirect_to :back
+  end
 
   private
 
